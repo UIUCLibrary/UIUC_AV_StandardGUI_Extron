aaee4eb10a63a6656b7bc5e89e46192416b63c47
Neil Feuerhelm <feuerhe1@illinois.edu>
2023-03-02 14:55:20 -0600

    fixes an issue with ranges on utility function subtests

--- unittests/test_utilityFunctions.py
+++ unittests/test_utilityFunctions.py
@@ -7,14 +7,14 @@ import utilityFunctions
 class UtilityFunctions_TimeIntToStr_TestCase(unittest.TestCase):
     
     def test_TimeIntToStr_unitless(self):
+        
         test_values = [42, 155, 4897, 94518]
         expected = ['0:00:00:42','0:00:02:35','0:01:21:37','1:02:15:18']
         
-        for i in range(0,3):
+        for i in range(len(test_values)):
             with self.subTest(i=i):
-                self.assertEqual(utilityFunctions.TimeIntToStr(test_values[i],
-                                                               False),
-                             expected[i])
+                testOutput = utilityFunctions.TimeIntToStr(test_values[i], False)
+                self.assertEqual(testOutput, expected[i])
         
     def test_TimeIntToStr_units(self):
         test_values = [1, 42, 117, 4897, 93678, 195273]
@@ -28,10 +28,9 @@ class UtilityFunctions_TimeIntToStr_TestCase(unittest.TestCase):
                 '2 days, 6 hours, 14 minutes, 33 seconds'
             ]
         
-        for i in range(0,5):
+        for i in range(len(test_values)):
             with self.subTest(i=i):
-                self.assertEqual(utilityFunctions.TimeIntToStr(test_values[i]),
-                             expected[i])
+                self.assertEqual(utilityFunctions.TimeIntToStr(test_values[i]), expected[i])
         
 if __name__ == '__main__':
     unittest.main()
